8c24891fe5821f73186ed3b849160aa4876a19df
yadavrajneesh481 <yadavrajneesh481@gmail.com>
2025-01-29 15:41:23 +0530

    Initial commit

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/yadavrajneesh481"
+                    className="text-primary underline underline-offset-1"
+                >
+                    yadavrajneesh481
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
